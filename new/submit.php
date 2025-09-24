32514dc641facac05b94ed3d26e1b72e963924ca
Nicholas656 <nicholas.goryczka@dawsoncollege.qc.ca>
2025-09-24 13:13:14 -0400

    php form testing

--- new/submit.php
+++ new/submit.php
@@ -1,2 +1,20 @@
 <p>GET: <?= var_dump($_GET) ?></p>
 <p>POST: <?= var_dump($_POST) ?></p>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Submission</title>
+</head>
+<body>
+    <?php
+    if(isset($_POST['reason']))
+    {
+        $reason=htmlspecialchars($_POST['reason']);
+        echo "Reason for submission: {$reason}";
+
+        
+    }
+    ?>
+</body>
+</html>

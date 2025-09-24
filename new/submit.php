3be0eed4ecd5f47078f90261bd3e3471cfa1a47a
Nicholas656 <nicholas.goryczka@dawsoncollege.qc.ca>
2025-09-24 13:20:19 -0400

    Update submit.php

--- new/submit.php
+++ new/submit.php
@@ -11,9 +11,14 @@
     if(isset($_POST['reason']))
     {
         $reason=htmlspecialchars($_POST['reason']);
+        $email=htmlspecialchars($_POST['email']);
+        $additional=htmlspecialchars($_POST['additional']);
         echo "Reason for submission: {$reason}";
-
-        
+        echo "<br>";
+        echo "Email address: {$email}";
+        echo "<br><br>";
+        echo "Additional information:";
+        echo "{$additional}";
     }
     ?>
 </body>
